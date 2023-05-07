111265430a1b6590de2499623e49b889b7168c1b
LynxFromSchool <lunarmhd@gmail.com>
2023-05-07 20:40:46 +0100

    fixed jump

--- RealAex/Assets/PlayerMovement.cs
+++ RealAex/Assets/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     public float moveSpeed;
     public float JumpPower;
+    public float maxjumps;
+    float jumps;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +24,16 @@ public class PlayerMovement : MonoBehaviour
            rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumps != 0)
         {
+            jumps -= 1;
+            // this is so if you double jump the jump isnt small and its the same as the normal one
+            rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(Vector3.up * JumpPower);
         }
 
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        jumps = maxjumps;
     }
 }
